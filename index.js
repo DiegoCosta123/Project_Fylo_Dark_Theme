f59358b9348c8e4a72f7e129ef1b100777ab7109
Diego <diegoccpinto@gmail.com>
2023-01-31 13:15:57 -0300

    ajustando a logica do JS em mensagem error

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const email = document.getElementById("email");
-const button_email = document.getElementById("button_content");
+const buttonEmail = document.getElementById("button_content");
+
+const errorMessage = document.getElementsByClassName("requiredField");
+
+
+function buttonEmail() {
+    if (email.value == '') {
+        document.querySelector(".error-name").textContent = "email obrigatório";
+        document.querySelector(".error-name").style.display = "block";
+        return;
+    }
+}
+
+// buttonEmail.addEventListener("click", (event) => {
+
+//     event.preventDefault();
+
+//     if (email.value == '') {
+//         document.querySelector(".error-name").textContent = "email obrigatório";
+//         document.querySelector(".error-name").style.display = "block";
+//         return;
+//     }
+// })
 
